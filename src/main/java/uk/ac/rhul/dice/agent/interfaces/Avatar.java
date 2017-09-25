144fb5737d0658b4e8e5ec7200b49e65dee400ee
cloudstrife9999 <cloudstrife9999@tiscali.it>
2017-09-25 21:05:01 +0100

    Minor.

--- src/main/java/uk/ac/rhul/dice/agent/interfaces/Avatar.java
+++ src/main/java/uk/ac/rhul/dice/agent/interfaces/Avatar.java
@@ -2,4 +2,5 @@ package uk.ac.rhul.dice.agent.interfaces;
 
 public interface Avatar extends ActiveBody {
     public abstract PrincipalListener getPrincipalListener();
+    public abstract void sendFeedbackToPrincipal(Analyzable... feedback);
 }
